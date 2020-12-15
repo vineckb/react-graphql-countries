6afa3393ae1670cf2c383f932d0dcd74bc842bb5
Vinicius Borges <vineckb@gmail.com>
2020-12-15 17:10:08 -0300

    proto: fix description on key up filter input test

--- src/components/Filter/index.test.js
+++ src/components/Filter/index.test.js
@@ -2,4 +2,4 @@ import { render } from '@testing-library/react'
 import Component from '.'
 
 test('should have input element', () => render(<Component />))
-test('should mutate state.filterString on input keyup', () => render(<Component />))
+test('should mutate state.filters.name on input keyup', () => render(<Component />))
