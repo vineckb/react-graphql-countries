cf1efcc10b8da1a40bca2f912f31c14f07b94bc2
Vinicius Borges <vineckb@gmail.com>
2020-12-15 17:01:52 -0300

    feat: add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomePage from './pages/Home'
+import CountryPage from './pages/Country'
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/country/:numericCode">
+            <CountryPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
