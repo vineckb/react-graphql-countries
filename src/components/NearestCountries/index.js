3ee85dcf3877c54c32b74f8ca5d928cf90d06076
Vinicius Borges <vineckb@gmail.com>
2020-12-16 23:58:12 -0300

    feat: add popup with distance between countries in mapbox

--- src/components/NearestCountries/index.js
+++ src/components/NearestCountries/index.js
@@ -10,27 +10,33 @@ mapbox.accessToken = 'pk.eyJ1IjoidmluZWNrYiIsImEiOiJja2lzNWhtNXUwcHFnMnFwNDV5MHZ
 
 export default function NearestCountries({ country }) {
   const node = useRef(null)
+  const map = useRef(null)
   const names = country.distanceToOtherCountries.map(item => item.countryName)
-  const { data } = useQuery(query, { variables: { names } })
+  const { data, loading } = useQuery(query, { variables: { names } })
 
   useEffect(() => {
-    const map = new mapbox.Map({
+    map.current = new mapbox.Map({
       container: node.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [country.location.longitude, country.location.latitude],
       zoom: 2
     })
+  }, [country])
 
-    if (!data) return
-
-    country.distanceToOtherCountries.forEach(({ countryName }) => {
+  useEffect(() => {
+    if (loading) return
+    country.distanceToOtherCountries.forEach(({ countryName, distanceInKm }) => {
       const item = data.Country.find(item => item.name === countryName)
-
+      const popup = new mapbox.Popup({ offset: 25 }).setHTML(`
+        <h3>${countryName}</h3>
+        <p>Distance from ${country.name}: ${Math.ceil(distanceInKm)} km</p>
+      `)
       new mapbox.Marker()
         .setLngLat([item.location.longitude, item.location.latitude])
-        .addTo(map)
+        .setPopup(popup)
+        .addTo(map.current)
     })
-  }, [country, data])
+  }, [data, loading, map, country])
 
   return (
     <div>
